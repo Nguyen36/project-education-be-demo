de4c9f910361679e2825bc00251885b9c1e756da
Nguyennk10 <nguyennk10@fpt.com>
2025-12-14 17:01:59 +0700

    fix

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -89,6 +89,7 @@ const checkPaymentVnpay = async (req, res) => {
         student.course_name,
         new Date().toISOString(),
         "PAID",
+        student.amount
       ]);
 
       return res.status(200).send(
